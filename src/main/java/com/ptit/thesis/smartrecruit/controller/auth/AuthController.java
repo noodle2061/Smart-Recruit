86e304c3ccb67f3f7afd228e456e954f028411c6
khanhnv8 <vankhanhboysss2601@gmail.com>
2025-10-06 22:13:15 +0700

    chore: remove unused libraries

--- src/main/java/com/ptit/thesis/smartrecruit/controller/auth/AuthController.java
+++ src/main/java/com/ptit/thesis/smartrecruit/controller/auth/AuthController.java
@@ -2,15 +2,12 @@ package com.ptit.thesis.smartrecruit.controller.auth;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ptit.thesis.smartrecruit.dto.request.OAuthRegisterRequest;
 import com.ptit.thesis.smartrecruit.dto.request.RegisterRequest;
 import com.ptit.thesis.smartrecruit.dto.response.ApiResponse;
 import com.ptit.thesis.smartrecruit.dto.response.UserResponse;
