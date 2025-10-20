6af552e6d1881d6210b19cdcb9ae332a9317ab68
khanhnv8 <vankhanhboysss2601@gmail.com>
2025-10-20 16:02:21 +0700

    feat/db-refactor: change logic auth module follow new schema

--- src/main/java/com/ptit/thesis/smartrecruit/mapper/UserMapper.java
+++ src/main/java/com/ptit/thesis/smartrecruit/mapper/UserMapper.java
@@ -2,8 +2,10 @@ package com.ptit.thesis.smartrecruit.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import com.ptit.thesis.smartrecruit.dto.response.UserResponse;
+import com.ptit.thesis.smartrecruit.entity.CandidateProfile;
 import com.ptit.thesis.smartrecruit.entity.User;
 
 @Mapper(componentModel = "spring")
@@ -11,10 +13,19 @@ public interface UserMapper {
 
     @Mapping(target = "firebaseUid", source = "userFirebaseUid")
     @Mapping(target = "userName", source = "username")
-    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "role", source = "role.roleName")
+    @Mapping(target = "fullName", source = "candidateProfile", qualifiedByName = "getFullName")
     @Mapping(target = "firebaseCustomToken", ignore = true)
     UserResponse toUserResponse(User user);
 
-    @Mapping(target = "userRoles", ignore = true)
+    @Mapping(target = "role", ignore = true)
     User toUserEntity(UserResponse userResponse);
+
+    @Named("getFullName")
+    default String getFullName(CandidateProfile candidateProfile) {
+        if (candidateProfile != null) {
+            return candidateProfile.getFullName();
+        }
+        return null;
+    }
 }
