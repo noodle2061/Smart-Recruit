8fa18bf4a44a0f5751965ad983ae0cdd9ce134fc
khuong.hm <minhkhuong782k3@gmail.com>
2026-01-07 02:06:36 +0700

    chore: noti comment

--- src/main/java/com/ptit/thesis/smartrecruit/enums/NotificationType.java
+++ src/main/java/com/ptit/thesis/smartrecruit/enums/NotificationType.java
@@ -4,5 +4,6 @@ public enum NotificationType {
     NEW_MESSAGE, // tin nhắn mới cho cả candidate và company
     JOB_APPLICATION, // thông báo cho trạng thái đơn ứng tuyển, cho cả candidate và company
     APPLICATION_STATUS_CHANGE, // trạng thái duyệt hay không duyệt, cho candidate
-    SYSTEM_ALERT // thông báo hệ thống, sự kiện,...
+    SYSTEM_ALERT, // thông báo hệ thống, sự kiện,...
+    NEW_COMMENT
 }
